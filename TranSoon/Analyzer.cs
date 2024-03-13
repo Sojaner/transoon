c072acb875737e8369f8c7e72f069c3178414b3d
Rojan Gharibpour <sojaner@gmail.com>
2024-03-13 11:56:14 +0100

    Added progress

--- TranSoon/Analyzer.cs
+++ TranSoon/Analyzer.cs
@@ -2,6 +2,7 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using ShellProgressBar;
 
 namespace TranSoon;
 
@@ -14,13 +15,44 @@ internal partial class Analyzer(Regex translatable, ITranslator translator, bool
         if (!Directory.Exists(folderPath))
         {
             Console.WriteLine($"Folder {folderPath} does not exist.");
+
             return;
         }
 
         string[] csFiles = Directory.GetFiles(folderPath, "*.cs", SearchOption.AllDirectories);
 
+        int totalTicks = csFiles.Length;
+
+        ProgressBarOptions options = new ()
+        {
+            ForegroundColor = ConsoleColor.Yellow,
+
+            BackgroundColor = ConsoleColor.DarkYellow,
+
+            ProgressCharacter = '─',
+
+            CollapseWhenFinished = false
+        };
+
+        ProgressBar progressBar = new(totalTicks, folderPath, options);
+
         foreach (string file in csFiles)
         {
+            ProgressBarOptions childOptions = new()
+            {
+                ForegroundColor = ConsoleColor.Green,
+
+                BackgroundColor = ConsoleColor.DarkGreen,
+
+                ProgressCharacter = '─',
+
+                CollapseWhenFinished = true
+            };
+
+            string filePath = Path.GetRelativePath(folderPath, file);
+
+            ChildProgressBar childProgressBar = progressBar.Spawn(1, filePath, childOptions);
+
             string code = await File.ReadAllTextAsync(file);
             SyntaxTree tree = CSharpSyntaxTree.ParseText(code);
             CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
@@ -32,78 +64,106 @@ internal partial class Analyzer(Regex translatable, ITranslator translator, bool
 
             if (nodes.Count == 0)
             {
-                Console.WriteLine($"No comments found in {file}");
+                childProgressBar.Tick();
+
+                progressBar.Tick();
+
                 continue;
             }
 
-            foreach (SyntaxTrivia node in nodes.Where(node => _shouldTranslate(node.ToFullString())))
+            try
             {
-                if (node.IsKind(SyntaxKind.SingleLineDocumentationCommentTrivia))
+                foreach (SyntaxTrivia node in nodes.Where(node => _shouldTranslate(node.ToFullString())))
                 {
-                    string documentation = node.ToFullString();
-
-                    MatchCollection matches = DocumentationCommentLine().Matches(documentation);
-
-                    List<string> outputs = [];
-
-                    foreach (Match match in matches.Cast<Match>())
+                    if (node.IsKind(SyntaxKind.SingleLineDocumentationCommentTrivia))
                     {
-                        string text = match.Groups["content"].Value;
+                        string documentation = node.ToFullString();
 
-                        string translation = _shouldTranslate(text) ? CapitalizeFirstLetter(await translator.TranslateAsync(text), capitalizeFirstLetter) : text;
+                        MatchCollection matches = DocumentationCommentLine().Matches(documentation);
 
-                        outputs.Add($"{match.Groups["space"]}///{match.Groups["between"]}{translation}{match.Groups["end"]}");
+                        List<string> outputs = [];
+
+                        foreach (Match match in matches.Cast<Match>())
+                        {
+                            string text = match.Groups["content"].Value;
+
+                            string translation = _shouldTranslate(text)
+
+                                ? CapitalizeFirstLetter(await translator.TranslateAsync(text), capitalizeFirstLetter)
+
+                                : text;
+
+                            outputs.Add( $"{match.Groups["space"]}///{match.Groups["between"]}{translation}{match.Groups["end"]}");
+                        }
+
+                        string result = string.Join("", outputs);
+
+                        code = code.Replace(documentation, result);
                     }
-
-                    string result = string.Join("", outputs);
-
-                    code = code.Replace(documentation, result);
-                }
-                else if (node.IsKind(SyntaxKind.SingleLineCommentTrivia))
-                {
-                    string comment = node.ToFullString();
-
-                    Match match = SingleLineComment().Match(comment);
-
-                    string content = match.Groups["content"].Value;
-
-                    string translation = _shouldTranslate(comment) ? CapitalizeFirstLetter(await translator.TranslateAsync(content), capitalizeFirstLetter) : content;
-
-                    string result = $"{match.Groups["space"]}{match.Groups["between"]}{translation}";
-
-                    code = code.Replace(comment, result);
-                }
-                else if (node.IsKind(SyntaxKind.MultiLineCommentTrivia))
-                {
-                    string comment = node.ToFullString();
-
-                    Match whole = WholeMultiLineComment().Match(comment);
-
-                    MatchCollection matches = MultiLineCommentLine().Matches(whole.Groups["content"].Value);
-
-                    List<string> outputs = [];
-
-                    foreach (Match match in matches.Cast<Match>())
+                    else if (node.IsKind(SyntaxKind.SingleLineCommentTrivia))
                     {
-                        string text = match.Groups["content"].Value;
+                        string comment = node.ToFullString();
 
-                        string translation = _shouldTranslate(text) ? CapitalizeFirstLetter(await translator.TranslateAsync(text), capitalizeFirstLetter) : text;
+                        Match match = SingleLineComment().Match(comment);
 
-                        outputs.Add($"{match.Groups["space"]}{translation}");
+                        string content = match.Groups["content"].Value;
+
+                        string translation = _shouldTranslate(comment)
+
+                            ? CapitalizeFirstLetter(await translator.TranslateAsync(content), capitalizeFirstLetter)
+
+                            : content;
+
+                        string result = $"{match.Groups["space"]}{match.Groups["between"]}{translation}";
+
+                        code = code.Replace(comment, result);
                     }
+                    else if (node.IsKind(SyntaxKind.MultiLineCommentTrivia))
+                    {
+                        string comment = node.ToFullString();
 
-                    string result = $"{whole.Groups["start"]}/*{string.Join("", outputs)}{whole.Groups["end"]}*/";
+                        Match whole = WholeMultiLineComment().Match(comment);
 
-                    code = code.Replace(comment, result);
+                        MatchCollection matches = MultiLineCommentLine().Matches(whole.Groups["content"].Value);
+
+                        List<string> outputs = [];
+
+                        foreach (Match match in matches.Cast<Match>())
+                        {
+                            string text = match.Groups["content"].Value;
+
+                            string translation = _shouldTranslate(text)
+
+                                ? CapitalizeFirstLetter(await translator.TranslateAsync(text), capitalizeFirstLetter)
+
+                                : text;
+
+                            outputs.Add($"{match.Groups["space"]}{translation}");
+                        }
+
+                        string result = $"{whole.Groups["start"]}/*{string.Join("", outputs)}{whole.Groups["end"]}*/";
+
+                        code = code.Replace(comment, result);
+                    }
                 }
+
+                await File.WriteAllTextAsync(file, code);
+
+                childProgressBar.Tick();
+
+                progressBar.Tick();
             }
+            catch (Exception e)
+            {
+                childOptions.ForegroundColor = ConsoleColor.DarkRed;
 
-            await File.WriteAllTextAsync(file, code);
+                childOptions.CollapseWhenFinished = false;
 
-            Console.WriteLine($"Comments in {file} translated and saved.");
+                childProgressBar.Tick($"{filePath}: {e.Message}");
+
+                progressBar.Tick();
+            }
         }
-
-        Console.WriteLine("Translation completed.");
     }
 
     private static string CapitalizeFirstLetter(string input, bool capitalize)
